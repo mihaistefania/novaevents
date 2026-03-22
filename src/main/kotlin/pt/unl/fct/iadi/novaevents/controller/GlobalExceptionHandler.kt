f5d9ffec8905db23bb35d9118ca64d493075685e
Mihai Stefania <mihaistefania2002@yahoo.com>
2026-03-22 22:15:44 +0200

    fixes

--- src/main/kotlin/pt/unl/fct/iadi/novaevents/controller/GlobalExceptionHandler.kt
+++ src/main/kotlin/pt/unl/fct/iadi/novaevents/controller/GlobalExceptionHandler.kt
@@ -5,6 +5,9 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.validation.BindException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import pt.unl.fct.iadi.novaevents.model.EventType
 import java.util.*
 
 @ControllerAdvice
@@ -15,5 +18,25 @@ class GlobalExceptionHandler {
     fun handleNotFound(ex: NoSuchElementException, model: Model): String {
         model.addAttribute("message", ex.message)
         return "error/404"
+    }
+
+    @ExceptionHandler(BindException::class)
+    @ResponseStatus(HttpStatus.OK)
+    fun handleBindException(ex: BindException, model: Model): String {
+
+        model.addAttribute("eventForm", ex.target)
+        model.addAttribute("types", EventType.values())
+
+        return "events/form"
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.OK)
+    fun handleValidationException(ex: MethodArgumentNotValidException, model: Model): String {
+
+        model.addAttribute("eventForm", ex.bindingResult.target)
+        model.addAttribute("types", EventType.values())
+
+        return "events/form"
     }
 }
