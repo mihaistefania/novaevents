f246291dccd6587ebddaac9128c457cf09dc6d1b
Mihai Stefania <mihaistefania2002@yahoo.com>
2026-03-29 02:59:22 +0200

    fixes

--- src/main/kotlin/pt/unl/fct/iadi/novaevents/controller/dto/EventForm.kt
+++ src/main/kotlin/pt/unl/fct/iadi/novaevents/controller/dto/EventForm.kt
@@ -1,22 +1,20 @@
-package pt.unl.fct.iadi.novaevents.controller.dto
-
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import java.time.LocalDate
 
 class EventForm(
 
-    @field:NotBlank
+    @field:NotBlank(message = "Name is required")
     var name: String? = null,
 
-    @field:NotNull
+    @field:NotNull(message = "Date is required")
     var date: LocalDate? = null,
 
     var location: String? = null,
 
     var description: String? = null,
 
-    @field:NotNull
+    @field:NotNull(message = "Type is required")
     var typeId: Long? = null,
 
     var clubId: Long? = null
